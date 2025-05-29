944c26bced8954c57337e7375226e446f36be662
Chirag Singh <chiragdhouni20@gmail.com>
2025-05-30 01:09:33 +0530

    just testing

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -13,7 +13,7 @@ const Stars = () => {
     ref.current.rotation.x -= delta / 10;
     ref.current.rotation.y -= delta / 10;
   });
-
+  //tdfsd
   return (
     <group rotation={[0, 0, Math.PI / 4]}>
       <Points ref={ref} positions={sphere} stride={3} frustumCulled>
